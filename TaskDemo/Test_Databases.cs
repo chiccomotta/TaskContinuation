7f2ca3bfb93cc06e030492af4bf23214af3d5355
Cristiano Motta <cristiano.motta@hotmail.it>
2021-10-16 15:54:06 +0200

    Refactoring

---
+++ TaskDemo/Test_Databases.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Databases
+{
+    public class Test_Databases
+    {
+        public void Test_DB_Sync()
+        {
+            string connectionString = "";
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (var sqlCommand = new SqlCommand(sqlSelect, sqlConnection))
+                {
+                    using (var reader = sqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var data = reader[0].ToString();
+                        }
+                    }
+                }
+            }
+        }
+        
+        public async Task Test_DB_AsyncAwait()
+        {
+            string connectionString;
+            #region Assign connectionString
+            connectionString = "";
+            #endregion
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+
+                using (var sqlCommand = new SqlCommand(sqlSelect, sqlConnection))
+                {
+                    using (var reader = await sqlCommand.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                            var data = reader[0].ToString();
+                        }
+                    }
+                }
+            }
+        }
+       
+        public void Test_DB_Async()
+        {
+            string connectionString;
+            #region Assign connectionString
+            connectionString = "";
+            #endregion
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                var sqlCommand = new SqlCommand(sqlSelect, sqlConnection);
+                var callback = new AsyncCallback(DataAvailable);
+                var ar = sqlCommand.BeginExecuteReader(callback, sqlCommand);
+
+                ar.AsyncWaitHandle.WaitOne();
+            }
+        }
+
+        private static void DataAvailable(IAsyncResult ar)
+        {
+            var sqlCommand = ar.AsyncState as SqlCommand;
+            using (var reader = sqlCommand.EndExecuteReader(ar))
+            {
+                while (reader.Read())
+                {
+                    var data = reader[0].ToString();
+                }
+            }
+        }
+
+       
+        public void Test_DB_AsyncTask()
+        {
+            string connectionString;
+            #region Assign connectionString
+            connectionString = "";
+            #endregion
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            var sqlConnection = new SqlConnection(connectionString);
+            Task taskSqlConnection = sqlConnection.OpenAsync();
+
+            taskSqlConnection.ContinueWith((Task tx, object state) =>
+            {
+                var sqlConn = state as SqlConnection;
+
+                var sqlCommand = new SqlCommand(sqlSelect, sqlConn);
+                Task<SqlDataReader> taskDataReader = sqlCommand.ExecuteReaderAsync();
+                Task taskProcessData = taskDataReader.ContinueWith((Task<SqlDataReader> txx) =>
+                {
+                    using (var reader = txx.Result)
+                    {
+                        while (reader.Read())
+                        {
+                            var data = reader[0].ToString();
+                        }
+
+                        mre.Set();
+                    }
+                }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            }, sqlConnection, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            mre.WaitOne();
+        }
+        ManualResetEvent mre = new ManualResetEvent(false);
+    }
+}
